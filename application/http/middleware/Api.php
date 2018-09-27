7de83abb1f64f5ae5575af40981d647dca4225fd
iamxiaoma <395487290@qq.com>
2018-09-27 19:33:08 +0800

    引入easywechat扩展
    引入easywechat扩展

--- application/http/middleware/Api.php
+++ application/http/middleware/Api.php
@@ -6,8 +6,26 @@ class Api
 {
     public function handle($request, \Closure $next)
     {
-        echo 'api 中间件前置动作，设置跨域配置等</br>';
-        // 中间件的前置行为
-        return $next($request);
+        try{
+            $Origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
+
+            // 设置支持跨域请求
+            header('Access-Control-Max-Age:86400');
+            header('Access-Control-Allow-Origin:'.$Origin);
+            header('Access-Control-Allow-Credentials:true');
+            header('Access-Control-Allow-Headers:x-requested-with,content-type');
+            header("Access-Control-Allow-Headers: Token,Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With, If-Modified-Since, Last-Modified");
+            // 中间件的前置行为
+            // 配置跨域
+            header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS, DELETE');
+            if (strtoupper($_SERVER['REQUEST_METHOD']) == 'OPTIONS') {
+                echo 'hello, this is a options request';
+                exit;
+            }
+            echo 'api 中间件前置动作，设置跨域配置等</br>';
+            return $next($request);
+        }catch(\Exception $e){
+            echo result_exception($e->getCode(), $e->getMessage());
+        }
     }
 }
---
+++ application/middleware.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @author machaoyi
+ * 全局中间件定义
+ */
+return [
+    'api' => app\http\middleware\Api::class,
+];
